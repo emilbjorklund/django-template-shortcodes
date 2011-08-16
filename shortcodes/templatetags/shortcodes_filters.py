c091868c27bfec0e300e39915b6a5660e64a1084
Henrik Almer <henrik@agoodid.se>
2011-08-16 15:11:03 +0200

    Added the django-shortcodes app (http://code.google.com/p/django-shortcodes/) to begood.contrib
    Includes a shortcode for YouTube movies

---
+++ shortcodes/templatetags/shortcodes_filters.py
@@ -0,0 +1,13 @@
+from shortcodes import parser
+from django import template
+
+register = template.Library()
+
+def shortcodes_replace(value):
+  """
+  A filter for parsing a string on the format ``[shortcode keyword=value]``
+  using the shortcodes parser method.
+  """
+  return parser.parse(value)
+
+register.filter('shortcodes', shortcodes_replace)
