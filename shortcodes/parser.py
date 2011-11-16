c61a9cf6fb6be629ab45df6726eb79bf1abebfb1
Henrik Almer <henrik@agoodid.se>
2011-11-16 11:08:46 +0100

    Bugfix to shortcodes parser that cause an exception when trying to parse the string ’’[...]’’

--- shortcodes/parser.py
+++ shortcodes/parser.py
@@ -5,7 +5,10 @@ from begood.contrib.shortcodes import parsers
 from django.core.cache import cache
 
 def import_parser(name):
-  mod = __import__(name)
+  try:
+    mod = __import__(name)
+  except ValueError:
+    return None
   components = name.split('.')
   for comp in components[1:]:
     mod = getattr(mod, comp)
@@ -16,7 +19,7 @@ def parse(value, request):
   groups = ex.findall(value)
   pieces = {}
   parsed = value
-
+  
   for item in groups:
     cache_key = memcached_safe_key(item)
   
@@ -29,21 +32,14 @@ def parse(value, request):
     
     args['request'] = request
     try:
-      if cache.get(cache_key):
-        try:
-          parsed = re.sub(r'\[' + re.escape(item) + r'\]', cache.get(cache_key), parsed)
-        except:
-          pass
-      else:
-        module = import_parser('begood.contrib.shortcodes.parsers.' + name)
-        function = getattr(module, 'parse')
-        result = function(args)
-        try:
-          cache.set(cache_key, result, 3600)
-          parsed = re.sub(r'\[' + re.escape(item) + r'\]', result, parsed)
-        except:
-          pass
-    except ImportError:
+      module = import_parser('begood.contrib.shortcodes.parsers.' + name)
+      function = getattr(module, 'parse')
+      result = function(args)
+      try:
+        parsed = re.sub(r'\[' + re.escape(item) + r'\]', result, parsed)
+      except:
+        pass
+    except (ImportError, AttributeError):
       pass
   return parsed
 
