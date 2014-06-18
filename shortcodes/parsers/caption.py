3cc019d2bc7f8cad47f1c1fdc5f2ccab9a98d27a
Emil Björklund <emil.bjorklund@inuse.se>
2014-06-18 13:59:19 +0200

    Some better handling of figcaption.

--- shortcodes/parsers/caption.py
+++ shortcodes/parsers/caption.py
@@ -1,5 +1,6 @@
 from django.template import Template, Context
 from django.utils.safestring import mark_safe
+from django.conf import settings
 
 try:
     from bs4 import BeautifulSoup
@@ -15,22 +16,37 @@ def parse(tag_atts, tag_contents):
     """
     if not tag_atts:
         return ''
+        
     tag_atts = dict([(a[0], a[1]) for a in tag_atts])
     tag_atts['content'] = mark_safe(tag_contents)
 
     # Attempt to separate the text from the image, and put that in a figcaption
     # if text is present inside the tag instead of in the caption attribute.
+
     if tag_contents:
-        soup = BeautifulSoup(tag_contents)
-        inside_text = soup.get_text()
-        if inside_text != '':
-            image = soup.img.extract()
-            captionpara = BeautifulSoup('<p>'+ inside_text.strip() +'</p>')
-            tag_atts['content'] = mark_safe(str(image))
-            tag_atts['caption'] = mark_safe(str(captionpara))
+        # try to get a soup instance from any tag contents passed in:
+        try:
+            soup = BeautifulSoup(tag_contents)
+            # If there's anything else inside the tag, extract the text.
+            # (harsh but...)
+            inside_text = soup.get_text()
+
+            if inside_text != '':
+                # Get the actual image:
+                image = soup.img.extract()
+
+                # If there's no image, something is seriously weird.
+                if image:
+
+                    # Add image to content as markup string:
+                    tag_atts['content'] = mark_safe(str(image))
+                    # Add caption text as caption.
+                    tag_atts['caption'] = inside_text
+        except:
+            pass
             
 
     context = Context(tag_atts)
 
-    t = Template('<figure {% if id %}id="{{ id }}" {% endif %}{% if align %}align="{{ align }}" {% endif %}{% if width %}style="width: {{ width }}px"{% endif %}>{{ content|safe }}<figcaption>{{ caption|safe }}</figcaption></figure>')
+    t = Template('<figure {% if id %}id="{{ id }}" {% endif %}{% if align %}class="align-{{ align }}"{% endif %} {% if width %}style="width: {{ width }}px"{% endif %}>{{ content|safe }}<figcaption><p>{{ caption|safe }}</p></figcaption></figure>')
     return t.render(context)
